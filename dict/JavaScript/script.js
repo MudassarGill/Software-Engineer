fc1e7703ed42082aeab99d695f465a2371894dd9
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-01 21:15:42 -0700

    const or not changed but let and var can be changed

--- dict/JavaScript/script.js
+++ dict/JavaScript/script.js
@@ -2,5 +2,7 @@
 var AccountID = "123456";
 let IsLoggedIn = true;
 const UserName = "JohnDoe";
+
+var AccountID = "654321"; // reassigning variable
 // if we want to change any variable name we need to add var or let but const don't work and not change if init
 console.table([AccountID, IsLoggedIn, UserName]);
