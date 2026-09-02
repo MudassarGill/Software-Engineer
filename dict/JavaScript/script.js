a1a765166ef28b4fa705fb42718c4da5a367b10e
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-01 21:13:50 -0700

    console table in js

--- dict/JavaScript/script.js
+++ dict/JavaScript/script.js
@@ -2,3 +2,5 @@
 var AccountID = "123456";
 let IsLoggedIn = true;
 const UserName = "JohnDoe";
+// if we want to change any variable name we need to add var or let but const don't work and not change if init
+console.table([AccountID, IsLoggedIn, UserName]);
