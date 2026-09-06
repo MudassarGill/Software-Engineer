7d1e11609fc877c1da622f6e4eb5b3ec6922e9ac
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-06 02:46:36 -0700

    arrow function

--- dict/JavaScript/this.js
+++ dict/JavaScript/this.js
@@ -1,10 +1,21 @@
-const Users={
-    username:"John",
-    age:30,
-    isAdmin:true,
-    email:"john@example.com",
-    GreetingMassage:function(){
-        return `Hello, ${this.username}`;
-    }   
+// const Users={
+//     username:"John",
+//     age:30,
+//     isAdmin:true,
+//     email:"john@example.com",
+//     GreetingMassage:function(){
+//         return `Hello, ${this.username}`;
+//     }   
+// }
+// console.log(Users.GreetingMassage());
+
+
+
+const UserName=(name,age,id)=>{
+     console.log(`Hello, ${name} your age is ${age} and your id is ${id}`);
 }
-console.log(Users.GreetingMassage());
+
+UserName("John", 30, 1);
+
+const AddTwoNumbers=(num1,num2)=>num1+num2;
+console.log(AddTwoNumbers(5,10));
