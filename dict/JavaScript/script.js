e5612c4929608fbaf4f853ede6c50a5319359aa8
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-06 00:02:42 -0700

    half js ended

--- dict/JavaScript/script.js
+++ dict/JavaScript/script.js
@@ -4,5 +4,6 @@ let IsLoggedIn = true;
 const UserName = "JohnDoe";
 
 var AccountID = "654321"; // reassigning variable
+IsLoggedIn = false; // reassigning variable
 // if we want to change any variable name we need to add var or let but const don't work and not change if init
-console.table([AccountID, IsLoggedIn, UserName]);
+// console.table([AccountID, IsLoggedIn, UserName]);
