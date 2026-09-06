2f2acd48d9192dafa9e3ab4889541d57eb2c1aed
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-06 01:43:58 -0700

    values of function with objects

--- dict/JavaScript/function.js
+++ dict/JavaScript/function.js
@@ -1,6 +1,14 @@
-function MyUserName() {
-    const userName = "Mudassar";
-    console.log(`Hello, ${userName}`);
-}
+// function MyUserName() {
+//     const userName = "Mudassar";
+//     console.log(`Hello, ${userName}`);
+// }
 
-MyUserName();
+// MyUserName();
+
+
+const MynewArry=[200,300,400,500,600,700,800,900];
+function MyComputeArray(newarry){
+        return newarry
+}
+const computedArray =   MyComputeArray(MynewArry);
+console.log(computedArray[1]);
