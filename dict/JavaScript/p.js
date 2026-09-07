3d08517b66e69d24298d4f41c60da073a57499dd
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-06 21:05:50 -0700

    fuction with calcultor

--- dict/JavaScript/p.js
+++ dict/JavaScript/p.js
@@ -2,3 +2,7 @@ const num1 = 5;
 const num2 = 10;
 const CalculateSum = (num1,num2) => num1 + num2;
 console.log(CalculateSum(num1,num2));
+const CalculateDifference = (num1,num2) => num1 - num2;
+console.log(CalculateDifference(num1,num2));
+const CalculateProduct = (num1,num2) => num1 * num2;
+console.log(CalculateProduct(num1,num2));
