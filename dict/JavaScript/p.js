929285330a393d9dde72b04b37dbd4d820a63886
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-06 21:07:23 -0700

    news values aded

--- dict/JavaScript/p.js
+++ dict/JavaScript/p.js
@@ -1,8 +1,14 @@
-const num1 = 5;
-const num2 = 10;
-const CalculateSum = (num1,num2) => num1 + num2;
-console.log(CalculateSum(num1,num2));
-const CalculateDifference = (num1,num2) => num1 - num2;
-console.log(CalculateDifference(num1,num2));
-const CalculateProduct = (num1,num2) => num1 * num2;
-console.log(CalculateProduct(num1,num2));
+// const num1 = 5;
+// const num2 = 10;
+// const CalculateSum = (num1,num2) => num1 + num2;
+// console.log(CalculateSum(num1,num2));
+// const CalculateDifference = (num1,num2) => num1 - num2;
+// console.log(CalculateDifference(num1,num2));
+// const CalculateProduct = (num1,num2) => num1 * num2;
+// console.log(CalculateProduct(num1,num2));
+
+function CalculateSum(num1,num2){
+    return num1 + num2;
+
+}
+CalculateSum(5,10);
