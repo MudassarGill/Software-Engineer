c2ae8501b6310425cf88bac6a43a8c57a05f1cba
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-07 18:18:37 -0700

    loops with arrays

--- dict/JavaScript/p.js
+++ dict/JavaScript/p.js
@@ -14,32 +14,38 @@
 // console.log(CalculateSum(5,10));
 
 
-const UserLoggedIn = true;
-if(UserLoggedIn){
-    console.log("Welcome User");
-}
-else{
-    console.log("Please Log In");
-}
+// const UserLoggedIn = true;
+// if(UserLoggedIn){
+//     console.log("Welcome User");
+// }
+// else{
+//     console.log("Please Log In");
+// }
 
-const DayofWeek = "Monday";
-switch(DayofWeek){
-    case "Monday":
-        console.log("Today is Monday");
-        break;
-    case "Tuesday":
-        console.log("Today is Tuesday");
-        break;
-    case "Wednesday":
-        console.log("Today is Wednesday");
-        break;
-    case "Thursday":
-        console.log("Today is Thursday");
-        break;
-    case "Friday":
-        console.log("Today is Friday");
-        break;
-    // Add more cases as needed
-    default:
-        console.log("Invalid day");
+// const DayofWeek = "Monday";
+// switch(DayofWeek){
+//     case "Monday":
+//         console.log("Today is Monday");
+//         break;
+//     case "Tuesday":
+//         console.log("Today is Tuesday");
+//         break;
+//     case "Wednesday":
+//         console.log("Today is Wednesday");
+//         break;
+//     case "Thursday":
+//         console.log("Today is Thursday");
+//         break;
+//     case "Friday":
+//         console.log("Today is Friday");
+//         break;
+//     // Add more cases as needed
+//     default:
+//         console.log("Invalid day");
+// }
+
+const numbers = [1, 2, 3, 4, 5];
+// Using for loop
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
 }
