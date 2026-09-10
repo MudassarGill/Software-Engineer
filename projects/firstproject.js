67fd886fd376d58b9fe1e772c53b9148d0d13925
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-10 08:44:22 -0700

    eventlisner

--- projects/firstproject.js
+++ projects/firstproject.js
@@ -1,1 +1,6 @@
 const AllBtnSelector = document.querySelectorAll('.button');
+const Bodyselector=document.querySelector('body')
+for (let i = 0; i < AllBtnSelector.length; i++) {
+    AllBtnSelector[i].addEventListener('click', function () {
+        Bodyselector.style.backgroundColor = AllBtnSelector[i].innerHTML;
+    })
