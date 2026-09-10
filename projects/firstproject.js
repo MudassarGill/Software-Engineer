f8bad86958e8f0e7f13f4f6526689232900ecc26
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-10 09:46:31 -0700

    with for loop

--- projects/firstproject.js
+++ projects/firstproject.js
@@ -1,9 +1,10 @@
 const AllBtnSelector = document.querySelectorAll('.button');
 const Bodyselector = document.querySelector('body');
 
-AllBtnSelector.forEach(function (button) {
-    button.addEventListener('click', function (event) {
 
+
+for (let i = 0; i < AllBtnSelector.length; i++) {
+    AllBtnSelector[i].addEventListener('click', function (event) {
         if (event.target.id === 'btn1') {
             Bodyselector.style.backgroundColor = 'red';
         }
@@ -17,5 +18,24 @@ AllBtnSelector.forEach(function (button) {
             Bodyselector.style.backgroundColor = 'yellow';
         }
 
-    });
-});
+    })
+}
+
+// AllBtnSelector.forEach(function (button) {
+//     button.addEventListener('click', function (event) {
+
+//         if (event.target.id === 'btn1') {
+//             Bodyselector.style.backgroundColor = 'red';
+//         }
+//         else if (event.target.id === 'btn2') {
+//             Bodyselector.style.backgroundColor = 'blue';
+//         }
+//         else if (event.target.id === 'btn3') {
+//             Bodyselector.style.backgroundColor = 'green';
+//         }
+//         else if (event.target.id === 'btn4') {
+//             Bodyselector.style.backgroundColor = 'yellow';
+//         }
+
+//     });
+// });
