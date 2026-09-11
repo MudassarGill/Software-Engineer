ca3185c13264bbf5663c35dd653f09e3ad165a6d
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-11 09:07:45 -0700

    successfully run

--- projects/secondpro.js
+++ projects/secondpro.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('form')
 
+const result = document.querySelector('#result')
+
 form.addEventListener('submit', function(e) {
 
     e.preventDefault()
@@ -7,17 +9,15 @@ form.addEventListener('submit', function(e) {
     const weight = Number(document.querySelector('#weight').value)
     const height = Number(document.querySelector('#height').value)
 
-    const submit = document.querySelector('#submit')
-
     if (height <= 0 || isNaN(height)) {
 
-        submit.innerHTML = `Please add a valid height`
+        result.innerHTML = `Please add a valid height`
 
     }
 
     else if (weight <= 0 || isNaN(weight)) {
 
-        submit.innerHTML = `Please add a valid weight`
+        result.innerHTML = `Please add a valid weight`
 
     }
 
@@ -29,23 +29,22 @@ form.addEventListener('submit', function(e) {
 
         if (bmi < 18.6) {
 
-            console.log('Under weight')
+            result.innerHTML = `BMI: ${bmi} - Under weight`
 
         }
 
         else if (bmi >= 18.6 && bmi <= 24.9) {
 
-            console.log('Normal weight')
+            result.innerHTML = `BMI: ${bmi} - Normal weight`
 
         }
 
         else {
 
-            console.log('Over weight')
+            result.innerHTML = `BMI: ${bmi} - Over weight`
 
         }
 
-        submit.innerHTML = `BMI: ${bmi}`
     }
 
 })
