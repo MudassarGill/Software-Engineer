4250e9957c0132c4d1823a11b1532f6cd18e5954
MudassarGill <mudassarhussain6533@gmail.com>
2026-09-16 09:20:41 -0700

    add increae btn

--- projects/btn.js
+++ projects/btn.js
@@ -1,4 +1,11 @@
-const buttons=document.querySelector('.btn').addEventListener('click',function(e){
-    console.log(e);
-    
+let number = document.querySelector(".number");
+const increaseBtn = document.querySelector("#increase");
+const decreaseBtn = document.querySelector("#decrease");
+const resetBtn = document.querySelector("#reset");
+
+let currentNumber=0;
+increaseBtn.addEventListener('click',function(e){
+  currentNumber++;
+  number.textContent=currentNumber;
 })
+
